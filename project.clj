4058b41577eaf6d17ea62c59ddc45a9b3d2982ac
Anthony Grimes <i@raynes.me>
2011-12-06 18:54:58 -0600

    Update to 1.3.0.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject io "0.1.0-alpha2"
   :description "The innermost moon of Jupiter."
-  :dependencies [[clojure "1.2.0"]]
+  :dependencies [[clojure "1.3.0"]]
   :java-source-path "src/java"
   :source-path "src/clj")

f3bbfa17b3427e7d93daf521b3847855fd04f6c8
Alan Malloy <alan@malloys.org>
2012-07-25 17:30:49 -0700

    0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io "0.2.0-beta2"
+(defproject io "0.2.0"
   :description "The innermost moon of Jupiter."
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :java-source-paths ["src/java"]

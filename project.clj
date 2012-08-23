96102d0fade5ef6007138c8c0b7484cd3309a4bc
Anthony Grimes <i@raynes.me>
2012-08-22 20:21:11 -0500

    Inc version and compile to a specific source and such.

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
-(defproject io "0.2.0"
+(defproject io "0.2.1"
   :description "The innermost moon of Jupiter."
+  :url "https://github.com/flatland/io"
+  :license  {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :java-source-paths ["src/java"]
-  :source-paths ["src/clj"])
+  :source-paths ["src/clj"]
+  :javac-options ["-target" "1.5" "-source" "1.5"])
